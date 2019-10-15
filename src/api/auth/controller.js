8ebf5cf8894878d6a2b756fbdb1fe0099473b8d4
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-10-15 13:23:04 +0200

    [ADD] Expiration time

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -2,7 +2,7 @@ import { sign } from '../../services/jwt'
 import { success } from '../../services/response/'
 
 export const login = ({ user }, res, next) =>
-  sign(user.id)
+  sign(user.id, {expiresIn: 60 * 60 * 24})
     .then((token) => ({ token, user: user.view(true) }))
     .then(success(res, 201))
     .catch(next)
